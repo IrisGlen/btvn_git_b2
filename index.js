7c20487c64dd7f677035256821b28653476c63f7
ngoc_linh <dinhthingoclinh92@gmail.com>
2023-05-08 15:14:31 +0700

    update

--- index.js
+++ index.js
@@ -1,75 +1,89 @@
-// Vòng lặp 
-// Có 3 loại vòng lặp while, do .. while, for
-// Đề bài: cho 1 số tự nhiên bất kỳ, tính xem từ 0 đến số tự nhiên đó có tổng cộng bao nhiêu số chia hết cho 2
+// bai 1: Tinh luong 
+// input : nhập số ngày làm = a (kiểu dữ kiệu số)
+// luong 1 ngày = 100.000
+// xử lý: tiền lương lấy giá trị a * lương 1 ngày
+// output: xuất tiền lương
 
-// input : n bất kỳ
-// xử lý: lấy tất cả số từ 0 đến n, chia % 2 = 0 : chia hết cho 2
-// output: tổng có bn số chia hết cho 2
-
-// function tinhTongCacSoChiaHetCho2() {
-//     //lấy dữ liệu từ người dùng
-//     var soTuNhien = document.getElementById("soN").value * 1;
-//     console.log(soTuNhien);
-//     var tongSoTNChiaCho2 = 0;
-//     while (soTuNhien >= 0)
-//     {
-//         // kiểm tra có chia hết cho 2 khong
-//         if (soTuNhien % 2 == 0) {
-//             tongSoTNChiaCho2++;  
-//         }
-//         // xử lý tăng giảm bước nhảy
-//         soTuNhien--;
-//     }
-//     document.querySelector(".bg-success").innerHTML = tongSoTNChiaCho2;
-// }
-
-// cách 2
-function tinhTongCacSoChiaHetCho2() {
+function tinhLuong() {
     //lấy dữ liệu từ người dùng
-    var soTuNhien = document.getElementById("soN").value * 1;
-    console.log(soTuNhien);
-    var tongSoTNChiaCho2 = 0;
-    var soBatDau = 0;
-    var inRaKQ = "";
-    while (soBatDau <= soTuNhien)
-    {
-        // kiểm tra có chia hết cho 2 khong
-        if (soBatDau % 2 == 0) {
-            tongSoTNChiaCho2++; 
-            inRaKQ += " Count " + (soBatDau+1) + " : Số " +  soBatDau + " <br>"; 
-        }
-        // xử lý tăng giảm bước nhảy
-        soBatDau++;
-    }
-    document.querySelector(".bg-success").innerHTML = inRaKQ;
+    var soNgay = document.getElementById("soNgay").value * 1;
+    const luongMotNgay = 100000;
+    //xử lý
+    var ketQua = 0;
+    ketQua = soNgay * luongMotNgay;
+    //xuất ra màn hình
+    document.querySelector(".soNgay").innerHTML = "Tiền lương nhân viên: " + ketQua;
 }
 
-// Vòng lặp do while
-// đề bài: người dùng nhập số K
-// tính tổng ký số có được từ 0 đến K
-// (ví dụ K là 5, => tính tổng từ 0, 1, 2, 3, 4, 5)
-function tinhTongCacKySo() {
+// B2: Tính giá trị trung bình, nhập sau khi nhập vào 5 số thực
+// input : nhập 5 số thực a, b, c, d, e
+// xử lý: ket qua = (a+b+c+d+e)/5
+// output: xuất giá trình tb
+
+function tinhGiaTriTrungBinh() {
     //lấy dữ liệu từ người dùng
-    var soK = document.getElementById("soK").value * 1;
-    console.log(soK);
-    var tongKySo = 0;
+    var soMot = document.getElementById("soMot").value * 1;
+    var soHai = document.getElementById("soHai").value * 1;
+    var soBa = document.getElementById("soBa").value * 1;
+    var soBon = document.getElementById("soBon").value * 1;
+    var soNam = document.getElementById("soNam").value * 1;
+    // xử lý
+    var ketQua = 0;
+    ketQua = (soMot + soHai + soBa + soBon + soNam) / 5
+    //xuất ra màn hình
+    document.querySelector(".giaTriTB").innerHTML = "Giá trị trung bình của 5 số là: " + ketQua;
+}
+
+// B3: Quy đổi tiền từ USD -> VND
+// input : Nhập số tiền USD (kiểu dữ kiệu số)
+// tỷ giá USD = 23.500VND
+// xử lý: tiền VND = số tiền USD * tỷ giá USD 23.500VND
+// output: xuất tiền VN
+
+function quyDoiTien() {
+    //lấy dữ liệu từ người dùng
+    var soTienUSD = document.getElementById("soTienUSD").value * 1;
+    const giaQuyDoi = 23500;
     //xử lý
-    // cách 2, chay từ K về 0
-    // do {
-    //     // tongKySo = tongKySo + soK;
-    //     tongKySo +=  soK;
-    //     soK --;
-    // } while (soK >= 0 );
-    // //xuất dữ liệu 
-    // document.querySelector(".bg-danger").innerHTML = "Tổng các ký số: " + tongKySo;
-    
-    // cách 1: (chay tu 0 toi K)
-    var soBatDau = 0;
-    do {
-        // tongKySo = tongKySo + soK;
-        tongKySo +=  soBatDau;
-        soBatDau ++;
-    } while (soBatDau <= soK);
-    //xuất dữ liệu 
-    document.querySelector(".bg-danger").innerHTML = "Tổng các ký số: " + tongKySo;
+    var ketQua = 0;
+    ketQua = soTienUSD * giaQuyDoi;
+    //xuất ra màn hình
+    document.querySelector(".quyDoi").innerHTML = "Số tiền VND là: " + ketQua;
+}
+// B4: Tính chu vi, diện tích HCN
+// input : Nhập chiều dai, rộng (kiểu dữ kiệu số)
+// xử lý: chu vi = (chiều dài + chiều rộng)*2
+// dien tích = chiều dài * chiều rộng
+// output: xuất chu vi, diện tích
+
+function chuViDienTich() {
+    //lấy dữ liệu từ người dùng
+    var chieuDai = document.getElementById("dai").value * 1;
+    var chieuRong = document.getElementById("rong").value * 1;
+    //xử lý
+    var chuVi = 0;
+    var dienTich = 0;
+    chuVi = (chieuDai + chieuRong) * 2;
+    dienTich = chieuDai * chieuRong;
+    //xuất ra màn hình
+    document.querySelector(".chuVi").innerHTML = "Chu vi là: " + chuVi;
+    document.querySelector(".dienTich").innerHTML = "Diện Tích là: " + dienTich;
+}
+// B5: Nhập số có 2 chữ số => Tính tổng 2 chữ số đó
+// input : Nhập số có hai chữ số (kiểu dữ kiệu số)
+// xử lý: tong = chữ số hàng đơn vị + chữ số hàng chục
+// output: xuất tổng
+
+function tongHaiChuSo() {
+    //lấy dữ liệu từ người dùng
+    var soHaiChu = document.getElementById("soHaiChu").value * 1;
+    //xử lý
+    var ketQua = 0;
+    var chuSoHangDonVi = Number(soHaiChu % 10);
+    var chuSoHangChuc = Math.floor(soHaiChu / 10);
+    // console.log(chuSoHangDonVi);
+    // console.log(chuSoHangChuc);
+    ketQua = chuSoHangDonVi + chuSoHangChuc;
+    //xuất ra màn hình
+    document.querySelector(".tinhTong").innerHTML = "Tổng hai chữ số này là: " + ketQua;
 }
