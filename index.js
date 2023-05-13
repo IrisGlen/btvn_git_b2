2e976c1ddf1e73bcb7f2990eefb41068a300c3d5
ngoc_linh <dinhthingoclinh92@gmail.com>
2023-05-14 00:24:19 +0700

    add

--- index.js
+++ index.js
@@ -1,139 +1,89 @@
-// B1: Tim so nguyen duong nho nhat sao cho 1+2+3...+n>10000
-// input : Nhập số n (kiểu dữ kiệu số)
-// xử lý: chay while thoả đk 1+2+3...+n>10000
-// output: xuất kq
+// Bài 1: Tinh luong 
+// input : nhập số ngày làm = a (kiểu dữ kiệu số)
+// luong 1 ngày = 100.000
+// xử lý: tiền lương lấy giá trị a * lương 1 ngày
+// output: xuất tiền lương
 
-function timSoNguyenDuongN() {
+function tinhLuong() {
+    //lấy dữ liệu từ người dùng
+    var soNgay = document.getElementById("soNgay").value * 1;
+    const luongMotNgay = 100000;
     //xử lý
     var ketQua = 0;
-    var i = 1;
-    while (ketQua < 10000) {   
-        ketQua += i;
-        i++; // tăng i lên nếu không sẽ bị lặp vô hạn
-    }
-// console.log(ketQua);
-// console.log(i);
+    ketQua = soNgay * luongMotNgay;
     //xuất ra màn hình
-    document.querySelector(".soNguyenDuong").innerHTML = "Số nguyên dương nhỏ nhất thoả điều kiện là: " + i;
+    document.querySelector(".soNgay").innerHTML = "Tiền lương nhân viên: " + ketQua;
 }
 
+// B2: Tính giá trị trung bình, nhập sau khi nhập vào 5 số thực
+// input : nhập 5 số thực a, b, c, d, e
+// xử lý: ket qua = (a+b+c+d+e)/5
+// output: xuất giá trình tb
 
-// B2: tính tong
-// input : Nhập số x,n (kiểu dữ kiệu số)
-// xử lý: viet ham luy thua, chay vong lap
-// output: xuất kq
-function hamLuyThua(x,soMu) {
-    var ketQua = 1;
-    for (var i = 1; i<=soMu; i++)
-    {
-        ketQua = ketQua * x;
-    }
-    return ketQua;
+function tinhGiaTriTrungBinh() {
+    //lấy dữ liệu từ người dùng so 1, 2, 3, 4, 5
+    var soMot = document.getElementById("soMot").value * 1;
+    var soHai = document.getElementById("soHai").value * 1;
+    var soBa = document.getElementById("soBa").value * 1;
+    var soBon = document.getElementById("soBon").value * 1;
+    var soNam = document.getElementById("soNam").value * 1;
+    // xử lý
+    var ketQua = 0;
+    ketQua = (soMot + soHai + soBa + soBon + soNam) / 5
+    //xuất ra màn hình
+    document.querySelector(".giaTriTB").innerHTML = "Giá trị trung bình của 5 số là: " + ketQua;
 }
-function tinhTong() {
+
+// B3: Quy đổi tiền từ USD -> VND
+// input : Nhập số tiền USD (kiểu dữ kiệu số)
+// tỷ giá USD = 23.500VND
+// xử lý: tiền VND = số tiền USD * tỷ giá USD 23.500VND
+// output: xuất tiền VN
+
+function quyDoiTien() {
     //lấy dữ liệu từ người dùng
-    var soMu = document.getElementById("nhapSoN").value * 1;
-    var soX= document.getElementById("nhapSoX").value * 1;
+    var soTienUSD = document.getElementById("soTienUSD").value * 1;
+    const giaQuyDoi = 23500;
     //xử lý
     var ketQua = 0;
-    // ketQua = hamLuyThua(soX,soMu);
-    
-    for (var i = 1; i <=soMu; i++)
-    {
-        ketQua += hamLuyThua(soX,i);
-        // console.log(ketQua);
-    }
-    
+    ketQua = soTienUSD * giaQuyDoi;
     //xuất ra màn hình
-    document.querySelector(".tinhTong").innerHTML = "Tổng luỹ thừa là: " + ketQua;
+    document.querySelector(".quyDoi").innerHTML = "Số tiền VND là: " + ketQua;
 }
+// B4: Tính chu vi, diện tích HCN
+// input : Nhập chiều dai, rộng (kiểu dữ kiệu số)
+// xử lý: chu vi = (chiều dài + chiều rộng)*2
+// dien tích = chiều dài * chiều rộng
+// output: xuất chu vi, diện tích
 
-
-// B3: tính giai thừa
-// input : Nhập số n (kiểu dữ kiệu số)
-// xử lý: ket qua = chạy vòng for * kết quả vói nhau
-// output: xuất kq
-
-function tinhGiaiThua() {
+function chuViDienTich() {
     //lấy dữ liệu từ người dùng
-    var soN = document.getElementById("soN").value * 1;
-
+    var chieuDai = document.getElementById("dai").value * 1;
+    var chieuRong = document.getElementById("rong").value * 1;
     //xử lý
-    var ketQua = 1;
-    for (var i =1; i <=soN; i++)
-    {
-        ketQua = ketQua * i;
-    }
-
+    var chuVi = 0;
+    var dienTich = 0;
+    chuVi = (chieuDai + chieuRong) * 2;
+    dienTich = chieuDai * chieuRong;
     //xuất ra màn hình
-    document.querySelector(".giaiThua").innerHTML = "Giai thừa là: " + ketQua;
+    document.querySelector(".chuVi").innerHTML = "Chu vi là: " + chuVi;
+    document.querySelector(".dienTich").innerHTML = "Diện Tích là: " + dienTich;
 }
+// B5: Nhập số có 2 chữ số => Tính tổng 2 chữ số đó
+// input : Nhập số có hai chữ số (kiểu dữ kiệu số)
+// xử lý: tong = chữ số hàng đơn vị + chữ số hàng chục
+// output: xuất tổng
 
-// B4: in 10 the div
-// input : click button
-// xử lý: ket qua the 10 the, the chan background do, the le in background xanh
-// output: xuất kq
-
-function in10TheDiv() {
-
-    //xử lý
-    var ketQua = 1;
-    for (var i =1; i <=10; i++)
-    {
-        var node = document.createElement("div");
-        var textnode = document.createTextNode("the div");
-        node.appendChild(textnode);
-        if (i % 2 ==0)
-        {
-            document.querySelector(".inTheDiv").appendChild(node).style.background ="green" ;
-        } else {
-            document.querySelector(".inTheDiv").appendChild(node).style.background ="red" ;
-        } 
-    }
-}
-// B5: in so nguyen to
-// input : Nhập số n (kiểu dữ kiệu số)
-// xử lý: ket qua =in so nguyen to
-// output: xuất kq
-function kiem_tra_snt(n)
-{
-    // Biến cờ hiệu
-    var flag = true;
- 
-    // Nếu n bé hơn 2 tức là không phải số nguyên tố
-    if (n < 2){
-        flag = false;
-    }
-    else{
-        // lặp từ 2 tới n-1
-        for (var i = 2; i < n-1; i++)
-        {
-            if (n % i == 0){
-                flag = false;
-                break;
-            }
-        }
-    }
- 
-    // Kiểm tra biến flag
-    if (flag == true){
-        return flag;
-    }
-}
-function inSoNguyenTo() {
+function tongHaiChuSo() {
     //lấy dữ liệu từ người dùng
-    var giaiTriNhap = document.getElementById("giaiTriNhap").value * 1;
-
+    var soHaiChu = document.getElementById("soHaiChu").value * 1;
     //xử lý
-    var ketQua = "";
-    for (var i =1; i <=giaiTriNhap; i++)
-    {
-        if (kiem_tra_snt(i) == true)
-        {
-            ketQua = ketQua + i + " ";
-        }
-    }
-
+    var ketQua = 0;
+    var chuSoHangDonVi = Number(soHaiChu % 10);
+    var chuSoHangChuc = Math.floor(soHaiChu / 10);
+    // console.log(chuSoHangDonVi);
+    // console.log(chuSoHangChuc);
+    ketQua = chuSoHangDonVi + chuSoHangChuc;
     //xuất ra màn hình
-    document.querySelector(".inSoNguyenTO").innerHTML = "Các số nguyên tố là: " + ketQua;
+    document.querySelector(".tinhTong").innerHTML = "Tổng hai chữ số này là: " + ketQua;
 }
